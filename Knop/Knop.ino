2dbc437da18c4808e510167c27a2af13c5a08acd
matmis <matthijsmissiaen@hotmail.com>
2017-05-30 14:53:30 +0200

    Update Knop.ino

--- Knop/Knop.ino
+++ Knop/Knop.ino
@@ -1,22 +1,24 @@
 #include <SoftwareSerial.h>
 #include "Command.h"
 
-static const int PIN_LED = 13;
+static const int PIN_LED = 12;
+static const int PIN_BUTTON = 7;
 static const Command CMD_END(";");
 static const Command CMD_LED_ON("LedOn");
 static const Command CMD_LED_OFF("LedOff");
 
 SoftwareSerial mySerial(2, 3); // RX, TX
 String inputString = "";
-
+bool previousButtonState;
+bool ButtonState;
 void setup()
 {
   Serial.begin(9600);
   mySerial.begin(9600);
-
+  pinMode(PIN_BUTTON, INPUT);
   pinMode(PIN_LED, OUTPUT);
   inputString.reserve(50);
-
+  previousButtonState = digitalRead(PIN_BUTTON);
   Serial.println("Loading complete");
 }
 
@@ -26,6 +28,16 @@ void loop()
   //  if (Serial.available()) {
   //    mySerial.write(Serial.read());
   //  }
+  ButtonState = digitalRead(PIN_BUTTON);
+  if(previousButtonState != ButtonState){
+      if(!ButtonState){
+         mySerial.println("in");
+      }else{
+        mySerial.println("uit");
+      }  
+      previousButtonState = ButtonState;
+  }
+  
 
   while (mySerial.available()) {
     // get the new byte
@@ -33,16 +45,16 @@ void loop()
     inputString += inChar;
   }
 
-  if (CMD_END.inString(inputString)))
+  if (CMD_END.inString(inputString))
   {
-    if (CMD_LED_ON(inputString)))
+    if (CMD_LED_ON.inString(inputString))
     {
-      digitalWrite(ledPin, HIGH);
+      digitalWrite(PIN_LED, HIGH);
       Serial.println("Aan");
     }
-    else if (CMD_LED_OFF.inString(inputString)))
+    else if (CMD_LED_OFF.inString(inputString))
     {
-      digitalWrite(ledPin, LOW);
+      digitalWrite(PIN_LED, LOW);
       Serial.println("Uit");
     }
     inputString = "";
