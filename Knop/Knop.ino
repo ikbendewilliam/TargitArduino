3e49775283805ebbe4706b41db99f83d96403015
ikbendewilliam <ikbendewilliam@hotmail.com>
2017-06-20 11:07:15 +0200

    added comments

--- Knop/Knop.ino
+++ Knop/Knop.ino
@@ -1,84 +1,84 @@
 #include <SoftwareSerial.h>
 #include "Command.h"
 
-static const int PIN_LED = 12;
-static const int PIN_BUTTON = 7;
-static const Command CMD_END(";");
-static const Command CMD_LED_ON("LedOn");
-static const Command CMD_LED_OFF("LedOff");
-static const Command CMD_LED_FLASH("LedFlash");
-static const String STR_BUTTON_PRESSED = "BtnPressed";
-static const String STR_BUTTON_RELEASED = "BtnReleased";
+static const int PIN_LED = 12; // The pin where the led is connected on
+static const int PIN_BUTTON = 7; // The pin where the button is connected on
+// WARNING: The following commands need to be the same as the commands in the app
+static const Command CMD_END(";"); // The end of a command
+static const Command CMD_LED_ON("LedOn"); // The command when you need to turn on the led
+static const Command CMD_LED_OFF("LedOff"); // The command when you need to turn off the led
+static const Command CMD_LED_FLASH("LedFlash"); // The command when you need to start flashing the led
+static const String STR_BUTTON_PRESSED = "BtnPressed"; // The command to send when a button is pressed
+static const String STR_BUTTON_RELEASED = "BtnReleased"; // The command to send when a button is released
 
-SoftwareSerial mySerial(2, 3); // RX, TX
-String inputString = "";
-bool previousButtonState;
-bool buttonState;
-bool ledFlash = false;
-bool flashState = false;
-unsigned long previousMillis = 0;
-int interval = 2000; // for debugging
+SoftwareSerial mySerial(2, 3); // Create a Serial connection on other pins. Format (RX, TX)
+String inputString = ""; // The string that is received
+bool previousButtonState; // The previous state of the button
+bool buttonState; // The current state of the button
+bool ledFlash = false; // Whether the led is currently flashing
+bool flashState = false; // Whether the led is currently on (during flashing)
+unsigned long previousMillis = 0; // The time of the previous flash state change
+int interval = 500; // The time to flash on or off, this should be specified with the command
 
 void setup()
 {
-  Serial.begin(9600);
-  mySerial.begin(9600);
-  pinMode(PIN_BUTTON, INPUT);
-  pinMode(PIN_LED, OUTPUT);
-  inputString.reserve(50);
-  previousButtonState = digitalRead(PIN_BUTTON);
-  Serial.println("Loading complete");
+  Serial.begin(9600); // Initialize the serial for debugging at a baudrate of 9600
+  mySerial.begin(9600); // Initialize the serial for bt at a baudrate of 9600
+  pinMode(PIN_BUTTON, INPUT); // Set the pinmode of the button to input
+  pinMode(PIN_LED, OUTPUT); // Set the pinmode of the led to output
+  inputString.reserve(50); // Reserve space for the incoming string
+  previousButtonState = digitalRead(PIN_BUTTON); // read the current state and save it to previousButtonState
+  Serial.println("Loading complete"); // Show that the setup is done
 }
 
 void loop()
 {
-  buttonState = digitalRead(PIN_BUTTON);
-  if (previousButtonState != buttonState) {
-    if (!buttonState) {
-      mySerial.println(STR_BUTTON_PRESSED);
-    } else {
-      mySerial.println(STR_BUTTON_RELEASED);
+  buttonState = digitalRead(PIN_BUTTON); // Get the current buttonstate
+  if (previousButtonState != buttonState) { // If the state has changed
+    if (!buttonState) { // If the button is pressed
+      mySerial.println(STR_BUTTON_PRESSED); // Send over bt that the burron is pressed
+    } else { // If the button is released
+      mySerial.println(STR_BUTTON_RELEASED); // Send over bt that the burron is released
     }
-    previousButtonState = buttonState;
+    previousButtonState = buttonState; // Change the previous buttonState
   }
 
-  while (mySerial.available()) {
-    // get the new byte
-    char inChar = (char)mySerial.read();
-    inputString += inChar;
+  while (mySerial.available()) { // If there is data coming in from bt
+    char inChar = (char)mySerial.read(); // get the new byte
+    inputString += inChar; // Save this byte to the inputString
   }
 
-  if (CMD_END.inString(inputString))
+  if (CMD_END.inString(inputString)) // If the end of a command is found in the inputString
   {
-    if (CMD_LED_ON.inString(inputString))
+    if (CMD_LED_ON.inString(inputString)) // If the command led on is found in the inputString
     {
-      ledFlash = false;
-      digitalWrite(PIN_LED, HIGH);
+      ledFlash = false; // The led is not flashing
+      digitalWrite(PIN_LED, HIGH); // Turn the led on
       //Serial.println("Aan");
     }
-    else if (CMD_LED_OFF.inString(inputString))
+    else if (CMD_LED_OFF.inString(inputString)) // If the command led off is found in the inputString
     {
-      ledFlash = false;
-      digitalWrite(PIN_LED, LOW);
+      ledFlash = false; // The led is not flashing
+      digitalWrite(PIN_LED, LOW); // Turn the led off
       //Serial.println("Uit");
     }
-    else if (CMD_LED_FLASH.inString(inputString))
+    else if (CMD_LED_FLASH.inString(inputString)) // If the command led flash is found in the inputString
     {
-      ledFlash = true;
-      flashState = false;
-      interval = CMD_LED_FLASH.getFirstParameter(inputString);
+      ledFlash = true; // The led is flashing
+      flashState = false; // Turn the flash state off, this means the led will be on at the start
+      interval = CMD_LED_FLASH.getFirstParameter(inputString); // Get the interval from the command
       //Serial.println("Flash");
     }
-    inputString = "";
+    inputString = ""; // Reset the inputString
   }
 
-  if (ledFlash)
+  if (ledFlash) // If flashing
   {
-     unsigned long currentMillis = millis();
-     if (currentMillis - previousMillis >= interval) {
-          previousMillis = currentMillis;
-          flashState = !flashState;
-          digitalWrite(PIN_LED, flashState ? HIGH : LOW);          
-     }
+    unsigned long currentMillis = millis(); // Get the time the program is busy
+    if (currentMillis - previousMillis >= interval) { // Check if the intervaltime has passed
+      previousMillis = currentMillis; // Save this time
+      flashState = !flashState; // Change the flashState
+      digitalWrite(PIN_LED, flashState ? HIGH : LOW); // Turn the led on/off depending on flashstate
+    }
   }
 }
