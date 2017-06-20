7fe78437ce8dbd9403dde48a224e54c3e2a05a5b
ikbendewilliam <ikbendewilliam@hotmail.com>
2017-06-20 11:07:36 +0200

    added comments

--- Knop/Command.h
+++ Knop/Command.h
@@ -1,26 +1,27 @@
+// A class to check commands from a string
 class Command
 {
   private:
-    String _value;
+    String _value; // The command to search for
 
   public:
-    Command(const String value)
+    Command(const String value) // The constructor needs the command to search for
     {
-      _value = value;
+      _value = value; // Saves the command
     }
 
-    bool inString(const String string) const
+    bool inString(const String string) const // Check the given string for the command
     {
-      return string.indexOf(_value) >= 0;
+      return string.indexOf(_value) >= 0; // Return whether the command is found
     }
 
-    int getFirstParameter(const String string) const
+    int getFirstParameter(const String string) const // Get the first parameter from the command
     {
-      int commandPlace = string.indexOf(_value);
-      int braceOpen = string.indexOf('(', commandPlace + 1);
-      int braceClose = string.indexOf(')', braceOpen + 1);
-      String parameter = string.substring(braceOpen + 1,braceClose);
+      int commandPlace = string.indexOf(_value); // find the command
+      int braceOpen = string.indexOf('(', commandPlace + 1); // Get the index of (
+      int braceClose = string.indexOf(')', braceOpen + 1); // Get the index of )
+      String parameter = string.substring(braceOpen + 1, braceClose); // Get the string inbetween
       //Serial.println(parameter);
-      return parameter.toInt();
+      return parameter.toInt(); // Transform the string inbetween to an int
     }
 };
